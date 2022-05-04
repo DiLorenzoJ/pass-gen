1007b477436d370dabe45459341b91a8eeef88c5
Johnathan William Dilorenzo <98494239+DiLorenzoJ@users.noreply.github.com>
2022-05-03 20:30:34 -0400

    added js for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-// Assignment code here
+function generatePassword() {  
+  //define a variable consisting alphabets in small and capital letter  
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";  
+  
+  //specify the length for the new string  
+  var lenString = 7;  
+  var randomstring = '';  
 
+  //loop to select a new character in each iteration  
+  for (var i=0; i<lenString; i++) {  
+    var rnum = Math.floor(Math.random() * characters.length);  
+    randomstring += characters.substring(rnum, rnum+1);  
+  }  
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +26,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword());
